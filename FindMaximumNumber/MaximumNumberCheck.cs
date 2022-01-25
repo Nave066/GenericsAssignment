d00e1afac2587dde5e33aeaa9e6fa3659a47de79
Nave066 <jayamnaveen68@gmail.com>
2022-01-25 17:46:42 +0530

    Add project files.

---
+++ FindMaximumNumber/MaximumNumberCheck.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindMaximumNumber
+{
+    class MaximumNumberCheck
+    {
+        public static int MaximumIntegerNumber(int firstValue, int secondValue,int thirdValue)
+        {
+            if (firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) > 0)
+            {
+               // Console.WriteLine("The biggest Value is" +firstValue);
+                return firstValue;
+            }
+            else if (secondValue.CompareTo(firstValue) > 0 && secondValue.CompareTo(thirdValue) > 0)
+            {
+                //Console.WriteLine("The biggest Value is" + secondValue);
+                return secondValue;
+            }
+            else if (thirdValue.CompareTo(firstValue) > 0 && thirdValue.CompareTo(secondValue) > 0)
+            {
+                //Console.WriteLine("The biggest Value is" + thirdValue);
+                return thirdValue;            
+            }
+            throw new Exception("All the values are same");
+        }
+        public static double MaximumDoubleNumber(double firstValue, double secondValue, double thirdValue)
+        {
+            if (firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) > 0)
+            {
+                //Console.WriteLine("The biggest Value is" + firstValue);
+                return firstValue;
+            }
+            else if (secondValue.CompareTo(firstValue) > 0 && secondValue.CompareTo(thirdValue) > 0)
+            {
+                //Console.WriteLine("The biggest Value is" + secondValue);
+                return secondValue;
+            }
+            else if (thirdValue.CompareTo(firstValue) > 0 && thirdValue.CompareTo(secondValue) > 0)
+            {
+                //Console.WriteLine("The biggest Value is" + thirdValue);
+                return thirdValue;
+            }
+            throw new Exception("All the values are same");
+        }
+        public static string MaximumStringNumber(string firstString, string secondString, string thirdString)
+        {
+            if (firstString.CompareTo(secondString) > 0 && firstString.CompareTo(thirdString) > 0 || 
+                firstString.CompareTo(secondString) >= 0 && firstString.CompareTo(thirdString) > 0 ||
+                firstString.CompareTo(secondString) > 0 && firstString.CompareTo(thirdString) >= 0)
+            {
+               // Console.WriteLine("The biggest Value is" + firstValue);
+                return firstString;
+            }
+            if (secondString.CompareTo(firstString) > 0 && secondString.CompareTo(thirdString) > 0 ||
+                secondString.CompareTo(firstString) >= 0 && secondString.CompareTo(thirdString) > 0 ||
+                secondString.CompareTo(firstString) > 0 && secondString.CompareTo(thirdString) >= 0)
+            {
+                //Console.WriteLine("The biggest Value is" + secondValue);
+                return secondString;
+            }
+            if (thirdString.CompareTo(firstString) > 0 && thirdString.CompareTo(secondString) > 0 ||
+                thirdString.CompareTo(firstString) >= 0 && thirdString.CompareTo(secondString) > 0 ||
+                thirdString.CompareTo(firstString) > 0 && thirdString.CompareTo(secondString) >= 0)
+            {
+                //Console.WriteLine("The biggest Value is" + secondValue);
+                return thirdString;
+            }
+            throw new Exception("All the values are same");
+        }
+    }
+}
---
+++ GenericsDemo/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace GenericsDemo
+{
+    internal class Program
+    {
+        public static void Main(String[] args)
+        {
+            int[] intArray = { 1, 2, 3, 4, 5, };
+            double[] doubleArray = { 1.1, 2.2, 3.3, 4.4 };
+            char[] charArray = { 'H', 'E', 'L', 'L', 'O' };
+            Display(intArray);
+            Display(charArray);
+            Display(doubleArray);
+            Console.ReadKey();
+        }
+
+        //public static void Display<T>(T[] test)
+        //{
+        //    for (int i = 0; i < test.Length; i++)
+        //    {
+        //        Console.Write(test[i] + " ");
+        //    }
+        //    Console.WriteLine();
+        //}
+        public static void Display(int[] input)
+        {
+            foreach (int element in input)
+            {
+                Console.WriteLine(element);
+            }
+        }
+        public static void Display(char[] input)
+        {
+            foreach (char element in input)
+            {
+                Console.WriteLine(element);
+            }
+        }
+        public static void Display(double[] input)
+        {
+            foreach (double element in input)
+            {
+                Console.WriteLine(element);
+            }
+        }
+    }
+}
